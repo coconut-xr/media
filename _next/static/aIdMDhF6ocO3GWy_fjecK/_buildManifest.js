6f41e23ccde8a32b8c1f94db8fe26afdea1ac570
cc-bbohlender <85560643+cc-bbohlender@users.noreply.github.com>
2022-12-07 17:04:59 +0000

    Deploying to gh-pages from @ cocoss-org/co-media@b1046e24d5597896ccaec02df9296f33a32f8bc1 🚀

---
+++ _next/static/aIdMDhF6ocO3GWy_fjecK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/836-af5dca53b0a4559b.js","static/chunks/pages/index-4d9ceacfee2d2c95.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
